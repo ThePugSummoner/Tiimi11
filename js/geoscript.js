5e0e476f85096f8bae3a7ede8964261fed19f0d6
Ilia Zubov <97738404+ThePugSummoner@users.noreply.github.com>
2022-05-05 01:38:55 +0300

    font-family changed and added own pages

---
+++ js/geoscript.js
@@ -0,0 +1,374 @@
+let questions = [
+
+    {
+        text: "Mariaanien hauta on läntisellä Tyynellämerellä sijaitseva maailman syvin tunnettu syvänmeren hauta. Sen pituus on noin 2 550 kilometriä ja keskimääräinen leveys 69 kilometriä. Mariaanien hauta on muodostunut Tyynenmeren laatan subduktoituessa Filippiinien laatan alle. Haudan pohjalla vedessä vallitsee 1 086 baarin paine, joka on tuhatkertainen normaaliin ilmanpaineeseen verrattuna. Mikä on sen syvin kohta?",
+        imgFile: "../images/diver-108881_640.jpg",
+        answer: [
+            "n.11000m",
+            "n.9000m",
+            "n.8000m",
+            "n.10000m"]
+    },
+
+    {
+        text: "Tämä mausoleumi sijaitsee Agrassa, Intiassa. Mausoleumi rakennettiin vuosien 1632 ja 1648 välillä. Rakennuskompleksin muut rakennukset kuten moskeija ja portti valmistuivat vuoteen 1653 mennessä. Valkoisesta marmorista rakennetun mausoleumin lisäksi rakennuskompleksiin kuuluu reunustava muuri, puisto, portti, moskeija ja vierastalo. Mausoleumi on koristeltu arvokkailla kivireliefeillä ja -upotuksilla sekä kalligrafialla. Mikä sen nimi on?",
+        imgFile: "../images/cultural-tourism-5264542_640.jpg",
+        answer: [
+            "Shah Jahan",
+            "Mumtaz Mahal",
+            "Taj Mahal",
+            "Humayun"]
+    },
+
+    {
+        text: "Kheopsin pyramidi (myös Khufun pyramidi tai Suuri pyramidi) on suurin ja tunnetuin egyptiläisistä pyramideista. Neljännen dynastian faarao Kheops (Khufu) oletettavasti rakennutti pyramidin hautapaikakseen noin 2560 eaa. Se oli maailman korkein rakennus yli 3 800 vuoden ajan. Missä kaupungissa se on?",
+        imgFile: "../images/egypt-2133951_640.jpg",
+        answer: [
+            "Kairossa",
+            "Luxorissa",
+            "Aleksandriassa",
+            "Gizassa"]
+    },
+
+    {
+        text: "Eiffelin torni on Ranskan pääkaupungissa Pariisissa sijaitseva torni. Rakentajansa Gustave Eiffelin mukaan nimetty torni on Pariisin kaupungin symboli, Euroopan kuuluisimpia nähtävyyksiä ja koko Ranskan suosituin matkailukohde. Eiffel-torni on televisioantenni. Minkä korkuinen se on?",
+        imgFile: "../images/eiffel-tower-3349075_640.jpg",
+        answer: [
+            "390m",
+            "324m",
+            "420m",
+            "286m"]
+    },
+
+    {
+        text: "Petronas Twin Towers ovat vuonna 1999 valmistuneet 452 metrin korkuiset pilvenpiirtäjät. Ne olivat maailman korkeimmat rakennukset vuodesta 1999 vuoteen 2004 asti, jolloin Taipei 101 valmistui. Ne ovat yhä maailman korkeimmat kaksoistornit ja korkeimmat 1900-luvulla valmistuneet rakennukset. Missä maassa ne ovat?",
+        imgFile: "../images/malaysia-2289403_640.jpg",
+        answer: [
+            "Malesiassa",
+            "Indonesiassa",
+            "Thaimaassa",
+            "Vietnamissä"]
+    },
+
+    {
+        text: "Chichén Itzá on kuuluisin ja parhaiten kunnostettu esikolumbiaaninen arkeologinen alue ja historiallinen kaupunki Jukatanin niemimaalla. Sen rakentamisen aloittivat mayat. Chichén Itzá kuuluu Unescon maailmanperintöluetteloon. Tämä pyramidi tunnettu nimellä Kukulcanin temppeli. Missä maassa tämä pyramidi sijaitsee?",
+        imgFile: "../images/mexico-3774303_640.jpg",
+        answer: [
+            "Egyptissä",
+            "Meksikossa",
+            "Kiinassa",
+            "Kolumbiassa"]
+    },
+
+    {
+        text: "Parthenon on antiikkinen temppeli. Parthenonin rakennustyö alkoi vuonna 447 eaa., ja rakennus oli pääosin valmis vuonna 438 eaa., jolloin järjestettiin sen juhlalliset vihkiäiset. Koristelutyöt jatkuivat kuitenkin ainakin vuoteen 432 eaa. asti. Temppeli oli antiikin ensimmäinen ja suurin kokonaan marmorista rakennettu temppeli. Missä maassa se on? ",
+        imgFile: "../images/greece-1594689_640.jpg",
+        answer: [
+            "Kyproksessa",
+            "Turkissa",
+            "Italiassa",
+            "Kreikassa"]
+    },
+
+    {
+        text: "Kellotorni on 96,3 metriä korkea ja sijaitsee Westminsterissä Thamesjoen varrella. Se on muodostunut Lontoon ja koko Yhdistyneen kuningaskunnan symboliksi. Kellotornin suunnittelivat Charles Barry ja Augustus Pugin. Se valmistui vuonna 1856 rakennustöiden kestettyä 13 vuotta. Vuodesta 2012 alkaen sen virallinen nimi on ... torni.",
+        imgFile: "../images/big-ben-1143631_640.jpg",
+        answer: [
+            "BigBen",
+            "Elisabetin",
+            "Pyhän Tapanin",
+            "Lontoon"]
+    },
+
+    {
+        text: "Colosseum oli valtakunnan suurin amfiteatteri, ja sinne mahtui ilmeisesti yli 50 000 katsojaa. Colosseumilla järjestettiin sen ensimmäisten vuosisatojen aikana gladiaattoritaisteluita, metsästysnäytöksiä ja muita näytöksiä sekä teloituksia. Tämä on tunnetuin nähtävyys kohde Italiassa. Missä kaupungissa se on?",
+        imgFile: "../images/colosseum-3456772_640.jpg",
+        answer: [
+            "Venetsiassa",
+            "Napolissa",
+            "Roomassa",
+            "Milanossa"]
+    },
+
+    {
+        text: "Kalteva torni on italialaisen kaupungin tuomiokirkon kellotorni Piazza dei Miracolin alueella. Torni on tullut kuuluisaksi siitä, että se on kallellaan. Romaanista tyyliä edustavan tornin rakentaminen aloitettiin vuonna 1173. Sen kallistuminen aiheutti taukoja rakentamiseen, ja kellotapuli valmistui vasta vuonna 1370. Tornissa on seitsemän kirkonkelloa. Tornin kallistuminen saatiin pysäytettyä 1990-luvulla aloitetussa suoristushankkeessa. Missä kaupungissa kalteva torni sijaitsee?",
+        imgFile: "../images/italy-4681065_640.jpg",
+        answer: [
+            "Venetsiassa",
+            "Pisassa",
+            "Palermossa",
+            "Bolognassa"]
+    },
+//10 questions
+    {
+        text: "Japani koostuu yli 3 000 saaresta, joista suurimmat ovat Honshū, Hokkaidō, Kyūshū ja Shikoku. Valtaosa saarista on vuoristoisia ja monet myös tuliperäisiä. Japanin Alpit ovat vuoristo Japanin pääsaaren Honshūn keskiosassa. Japanin korkein vuoristo koostuu Hida-, Kiso- ja Akaishi-vuoristoista (Pohjois-, Keski- ja Etelä-Alpeista).Mikä on Japanin korkein vuori?",
+        imgFile: "../images/mount-fuji-6748510_640.jpg",
+        answer: [
+            "Kitadake",
+            "Hotakadake",
+            "Komagatake",
+            "Fuji"]
+    },
+
+    {
+        text: "Vuori on ympäröivää aluetta selvästi korkeampi pinnanmuoto. Vuorella on tyypillisesti jyrkät rinteet, suhteellisen helposti rajattavissa oleva huippualue ja huomattavat paikalliset korkeuserot. Vuoria muodostuu laattatektonisten mekanismien tai vulkaanisen toiminnan tuloksena. Mikä on maailman korkein vuori?",
+        imgFile: "../images/mount-everest-5295184_640.jpg",
+        answer: [
+            "Mont Blanc",
+            "Everest",
+            "K2",
+            "Aconcagua"]
+    },
+
+    {
+        text: "Tämä valtio sijaitsee Etelä-Amerikan eteläosassa Atlantin länsirannikolla. Se ulottuu trooppisesta pohjoisesta Etelä-Amerikan antarktiseen eteläkärkeen. Maan eläinpopulaatioon kuuluu niin apinoita, jaguaareja kuin pingviinejäkin. Virallinen kieli on espanja. Mikä valtio se on?",
+        imgFile: "../images/international-2690834_640.jpg",
+        answer: [
+            "Bolivia",
+            "Argentina",
+            "Peru",
+            "Chile"]
+    },
+
+    {
+        text: "Brasilia on Etelä-Amerikan suurin valtio sekä pinta-alaltaan että asukasluvultaan ja se on myös sekä pinta-alaltaan että asukasluvultaan maailman viidenneksi suurin valtio. Euroopan unionin alue vastaa vain puolta Brasilian pinta-alasta. Virallinen kieli on portugali. Mikä on Brasilian pääkaupunki?",
+        imgFile: "../images/flag-742573_640.jpg",
+        answer: [
+            "São Paulo",
+            "Rio de Janeiro",
+            "Brasilia",
+            "Blumenau"]
+    },
+
+    {
+        text: "Valtameret ovat maapallon suurimpia meriä. Kaikki valtameret ovat yhteydessä toisiinsa. Tavallisesti niitä määritellään olevan kolme, neljä tai viisi. Pohjoisella pallonpuoliskolla ja vielä päiväntasaajan seudulla mantereet kuitenkin erottavat ne selvästi toisistaan. Mikä on maailman suurin valtameri (pinta-ala)?",
+        imgFile: "../images/cruise-1578528_640.jpg",
+        answer: [
+            "Tyyni",
+            "Atlannin",
+            "Intian",
+            "Pohjoinen"]
+    },
+
+    {
+        text: "Petra perustettiin nabatealaisten pääkaupungiksi kuudennella vuosisadalla eaa. Se tunnetaan erityisesti kiveen kaiverretusta arkkitehtuuristaan ja vesijohtojärjestelmästään. Suurimmillaan Petrassa oli mahdollisesti yli 20 000 asukasta, ja se oli merkittävä kauppakeskus. Nykyisin Petra on suosittu vierailukohde, ja se on ollut Unescon maailmanperintöluettelossa vuodesta 1985. Missä maassa Petra sijaitsee?",
+        imgFile: "../images/petra-6294051_640.jpg",
+        answer: [
+            "Egyptissä",
+            "Turkissa",
+            "Jordaniassa",
+            "Syyriassa"]
+    },
+    //16 questions
+    {
+        text: "Scotlanti on maa Ison-Britannian saaren pohjois­osassa. Se käsittää suunnilleen kolmanneksen saaren pinta-alasta ja kuuluu Yhdistyneen kuningas­kunnan eli Britannian valtioon. Sen kokonais­pinta-ala on 78 772 neliökilometriä. Viralliset kielet ovat englanti, skotti ja gaeli. Mikä näistä on Scotlannin pääkaupunki?",
+        imgFile: "../images/flag-972353_640.jpg",
+        answer: [
+            "Aberdeen",
+            "Glasgow",
+            "Dundee",
+            "Edinburgh"]
+    },
+
+    {
+        text: "Joki on suuri luonnon muovaama maanuoma, jossa virtaa vettä. Useimpien jokien kuljettama vesi on peräisin yläville maille osuneista sateista. Joki voi saada lisää vettä myös lähteistä, jossa pohjavesi tulee esiin pintavedeksi. Aluetta, jolle satanut vesi valuu joen uomaan, kutsutaan valuma-alueeksi. Tämän alueen ympärille muodostuu vedenjakaja. Kumpi näistä on pisin joki?",
+        imgFile: "../images/river-3642456_640.jpg",
+        answer: [
+            "Mississippi",
+            "Kongo",
+            "Amazon",
+            "Mekong"]
+    }
+];
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+let num = 0;
+
+let count = 0;
+
+let submit = document.getElementById("btn6");
+
+submit.addEventListener("click", submitAnswer);
+
+let check1 = document.getElementById("btn1");
+let check2 = document.getElementById("btn2");
+let check3 = document.getElementById("btn3");
+let check4 = document.getElementById("btn4");
+
+check1.addEventListener("click", countAnswers1);
+check2.addEventListener("click", countAnswers2);
+check3.addEventListener("click", countAnswers3);
+check4.addEventListener("click", countAnswers4);
+
+
+let rightAnswers = [questions[0].answer[0],
+                    questions[1].answer[2],
+                    questions[2].answer[3],
+                    questions[3].answer[1],
+                    questions[4].answer[0],
+                    questions[5].answer[1],
+                    questions[6].answer[3],
+                    questions[7].answer[1],
+                    questions[8].answer[2],
+                    questions[9].answer[1],
+                    questions[10].answer[3],
+                    questions[11].answer[1],
+                    questions[12].answer[1],
+                    questions[13].answer[2],
+                    questions[14].answer[0],
+                    questions[15].answer[2],
+                    questions[16].answer[3],
+                    questions[17].answer[2]
+];
+
+function countAnswers1() {   
+        
+    for (let j = 0; j < rightAnswers.length; j++) {
+            
+        if (document.getElementById("btn1").innerHTML == rightAnswers[j]) {
+            count++;
+            document.getElementById("btn1").disabled = true;
+            document.getElementById("btn2").disabled = true;
+            document.getElementById("btn3").disabled = true;
+            document.getElementById("btn4").disabled = true;
+            document.getElementById("btn6").disabled = false;
+            document.getElementById("result").innerHTML = "<div class='alert alert-success' role='alert'>" + 
+            "Vastauksesi on oikein!" + "</div>";
+            check1.setAttribute("disable");
+            check2.setAttribute("disable");
+            check3.setAttribute("disable");
+            check4.setAttribute("disable");
+        }
+        else {
+            document.getElementById("result").innerHTML = "<div class='alert alert-danger' role='alert'>" + 
+            "Vastauksesi on väärin!" + "</div>";
+        }
+        document.getElementById("btn1").disabled = true;
+        document.getElementById("btn2").disabled = true;
+        document.getElementById("btn3").disabled = true;
+        document.getElementById("btn4").disabled = true;
+        document.getElementById("btn6").disabled = false;
+    }
+    return count;
+}
+
+function countAnswers2() {
+        
+    for (let j = 0; j < rightAnswers.length; j++) {
+            
+        if (document.getElementById("btn2").innerHTML == rightAnswers[j]) {
+            count++;
+            document.getElementById("result").innerHTML = "<div class='alert alert-success' role='alert'>" + 
+            "Vastauksesi on oikein!" + "</div>";
+            check1.setAttribute("disable");
+            check2.setAttribute("disable");
+            check3.setAttribute("disable");
+            check4.setAttribute("disable");
+        }
+        else {
+            document.getElementById("result").innerHTML = "<div class='alert alert-danger' role='alert'>" + 
+            "Vastauksesi on väärin!" + "</div>";
+        }
+        document.getElementById("btn1").disabled = true;
+        document.getElementById("btn2").disabled = true;
+        document.getElementById("btn3").disabled = true;
+        document.getElementById("btn4").disabled = true;
+        document.getElementById("btn6").disabled = false;
+    }
+    return count;
+}
+
+function countAnswers3() {
+
+    for (let j = 0; j < rightAnswers.length; j++) {
+                
+        if (document.getElementById("btn3").innerHTML == rightAnswers[j]) {
+            count++;
+            document.getElementById("result").innerHTML = "<div class='alert alert-success' role='alert'>" + 
+            "Vastauksesi on oikein!" + "</div>";
+            check1.setAttribute("disable");
+            check2.setAttribute("disable");
+            check3.setAttribute("disable");
+            check4.setAttribute("disable");
+        }
+        else {
+            document.getElementById("result").innerHTML = "<div class='alert alert-danger' role='alert'>" + 
+            "Vastauksesi on väärin!" + "</div>";
+        }
+        document.getElementById("btn1").disabled = true;
+        document.getElementById("btn2").disabled = true;
+        document.getElementById("btn3").disabled = true;
+        document.getElementById("btn4").disabled = true;
+        document.getElementById("btn6").disabled = false;
+    }
+    return count;
+}
+
+function countAnswers4() {
+                
+    for (let j = 0; j < rightAnswers.length; j++) {
+                    
+        if (document.getElementById("btn4").innerHTML == rightAnswers[j]) {
+            count++;
+            document.getElementById("result").innerHTML = "<div class='alert alert-success' role='alert'>" + 
+            "Vastauksesi on oikein!" + "</div>";
+            check1.setAttribute("disable");
+            check2.setAttribute("disable");
+            check3.setAttribute("disable");
+            check4.setAttribute("disable");
+        }
+        else {
+            document.getElementById("result").innerHTML = "<div class='alert alert-danger' role='alert'>" + 
+            "Vastauksesi on väärin!" + "</div>";
+        }
+        document.getElementById("btn1").disabled = true;
+        document.getElementById("btn2").disabled = true;
+        document.getElementById("btn3").disabled = true;
+        document.getElementById("btn4").disabled = true;
+        document.getElementById("btn6").disabled = false;
+    }
+    return count;
+}
+
+
+
+function submitAnswer() {
+
+    document.getElementById("result").innerHTML = "";
+
+    document.getElementById("btn1").disabled = false;
+    document.getElementById("btn2").disabled = false;
+    document.getElementById("btn3").disabled = false;
+    document.getElementById("btn4").disabled = false;
+    
+    num++;
+
+    if(num > 4) {
+        submit.addEventListener("click", countPoints);
+    }
+
+    let rndNumber = getRndInteger(0, questions.length - 1);
+    
+    for (let i = 0; i < questions.length; i++) {
+        document.getElementById("num").innerHTML = num;
+        document.getElementById("questionHere").innerHTML = questions[rndNumber].text;
+        document.getElementById("picture").innerHTML = "<img src ='"+ questions[rndNumber].imgFile +"' class='rounded mx-auto d-block my-5' alt=''/>";
+        document.getElementById("btn1").innerHTML = questions[rndNumber].answer[0];
+        document.getElementById("btn2").innerHTML = questions[rndNumber].answer[1];
+        document.getElementById("btn3").innerHTML = questions[rndNumber].answer[2];
+        document.getElementById("btn4").innerHTML = questions[rndNumber].answer[3];
+    }
+    questions.splice(rndNumber, 1);
+
+    document.getElementById("btn6").disabled = true;
+}
+
+function countPoints() {
+    if (count == 5)
+    document.getElementById("picture").innerHTML = "<img src ='../images/traveller-3213992_640.jpg' class='rounded mx-auto d-block my-5' alt=''/>";
+    document.getElementById("questionHere").innerHTML = "Olet vastanut kaikiin kysymyksiin oikein. Sinun pistemääräsi on " + count;
+    document.getElementById("header").innerHTML = "Tuloksesi";
+}
